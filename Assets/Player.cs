6003c4e7da8bedb8b70fbd82a5b65f67a311af6d
Michael King <mack178@hotmail.com>
2017-07-05 16:09:35 -0600

    Twin Sticks project started

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,17 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
-public class Script : MonoBehaviour {
+public class Player : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        print("Hello there");
-        print("How do you do?");
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        print(CrossPlatformInputManager.GetAxis("Horizontal"));
 	}
 }
